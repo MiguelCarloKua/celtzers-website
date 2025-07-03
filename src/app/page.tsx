d7c129d5b9077aa83922c2ffe83f74945d7f0cb2
MiguelCarloKua <miguel_carlo_kua@dlsu.edu.ph>
2025-07-03 21:16:23 +0800

    Bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#31255e] text-white font-sans">
       <section className="py-32 text-center">
-        <h1 className="text-4xl font-bold text-[#978bc4] mb-6">Welcome to CeLTZer's Digest Generator Tool!</h1>
+        <h1 className="text-4xl font-bold text-[#978bc4] mb-6">Welcome to CeLTZer&apos;s Digest Generator Tool!</h1>
         <p className="text-lg max-w-2xl mx-auto">
-          Generate clean, structured case digests from Lawphil in one click. Whether you’re a student or lawyer, we’ll help you summarize key legal decisions with confidence.
+          Generate clean, structured case digests from Lawphil in one click. Whether you&apos;re a student or lawyer, we&apos;ll help you summarize key legal decisions with confidence.
         </p>
       </section>
     </main>
